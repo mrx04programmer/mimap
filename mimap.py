9ad6bab52585581aa5737ea3b4f64f7dd62ed179
mrx04programmer <mjuandavid901@gmail.com>
2022-09-08 21:56:25 -0500

    Agregando modulo de detección de sniffing

--- mimap.py
+++ mimap.py
@@ -36,12 +36,15 @@ def mod_os():
     os.system("sudo nmap -T4 -O "+parse.ip+" -oG so_"+parse.ip)
 def spoofing():
     ipomac = input(B+"Que tipo de objetivo es (Mac/Ip)[M/i] ")
-    if ipomac == "Mac" or ipomac == "m" or ipomac == "M":
+    if "M" in ipomac or "m" in ipomac:
         action = input(C+"comandos a correr en "+parse.ip+" (ej: -O -T5 8.8.8.8) > "+W)
         os.system("nmap --spoof-mac "+parse.ip+" "+action)
     else:
         action = input(C+"comandos a correr en "+parse.ip+" (ej: -O -T5 8.8.8.8) > "+W)
         os.system("nmap -S "+parse.ip+" "+action)
+def detect_sniff():
+    print(f"{B}[*] {W}Detectando sniffing en {parse.ip}")
+    os.system(f"nmap -sV --script=sniffer-detect {parse.ip}")
 
 os.system("clear")
 banner()
@@ -53,16 +56,18 @@ if parse.list:
     scan : Escanear y encontrar puertos abiertos en el objetivo
     scanB : Escanear bruscamente para encontrar puertos abiertos en el objetivo
     spoofing : Correr otro modulo con señuelos para el ataque de evaciones
-    http : Ver peticiones HTTP de una IP 
+    detect-sniff : Detectar sniffing en una ip en especifico<
 
     """)
     exit()
 if parse.module == "os":
     mod_os()
     exit()
-if parse.module == "spoofing":
+elif parse.module == "spoofing":
     spoofing()
     exit()
-if parse.module == "scan":
+elif parse.module == "scan":
     print(G+"[X] "+W+"Buscando puertos en "+parse.ip)
     os.system("sudo ./s "+parse.ip)
+elif parse.module == "detect-sniff":
+    detect_sniff()
